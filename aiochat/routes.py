f0800a1ed74b14701ff8daa3363c46a028efbaf3
Maks <samael500@gmail.com>
2017-04-07 15:17:53 +0300

    :repeat: rename routes to urls

--- aiochat/routes.py
+++
@@ -1,7 +0,0 @@
-from views import Index
-from accounts.views import Login
-
-routes = (
-    dict(method='GET', path='/', handler=Index, name='index'),
-    dict(method='*', path='/login', handler=Login, name='login'),
-)
---
+++ aiochat/urls.py
@@ -0,0 +1,7 @@
+from views import Index
+from accounts.urls import routes as accounts_routes
+
+
+routes = (
+    dict(method='GET', path='/', handler=Index, name='index')
+) + accounts_routes
